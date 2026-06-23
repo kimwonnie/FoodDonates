3b1217474d4118f66ae6d57e5fc458f788f81059
Womhyung Kim <umluanaatico@gmail.com>
2026-06-23 20:51:17 -0300

    atualizações gerais

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,15 +3,13 @@ import env from "./env.js";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(env.MONGODB_URI);
+    await mongoose.connect(env.MONGO_URI, {
+      dbName: env.DB_NAME,
+    });
 
-    console.log("✅ MongoDB conectado");
+    console.log("📦 MongoDB conectado com sucesso");
   } catch (error) {
-    console.error(
-      "❌ Erro ao conectar MongoDB:",
-      error.message
-    );
-
+    console.error("❌ Erro ao conectar no MongoDB:", error.message);
     process.exit(1);
   }
 };
--- backend/config/env.js
+++ backend/config/env.js
@@ -3,20 +3,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const env = {
-  NODE_ENV: process.env.NODE_ENV || "development",
-
   PORT: process.env.PORT || 3000,
+  MONGO_URI: process.env.MONGO_URI,
+  DB_NAME: process.env.DB_NAME || "plataforma_solidaria",
 
-  MONGODB_URI:
-    process.env.MONGODB_URI ||
-    "mongodb://localhost:27017/plataforma-solidaria",
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1d",
 
-  JWT_SECRET:
-    process.env.JWT_SECRET ||
-    "plataforma_solidaria_2026",
-
-  JWT_EXPIRES_IN:
-    process.env.JWT_EXPIRES_IN || "1d"
+  NODE_ENV: process.env.NODE_ENV || "development",
 };
 
 export default env;
